efedacf5b7056d28a3cdfa88c1c64b72a38098bb
ShahdRaafat <shahdraafat.725@gmail.com>
2025-07-11 23:49:03 +0300

    Finish City View

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -1,3 +1,4 @@
+import { useCities } from "../contexts/CitiesContext";
 import styles from "./CityItem.module.css";
 import { Link } from "react-router-dom";
 const formatDate = (date) =>
@@ -11,10 +12,14 @@ const formatDate = (date) =>
 function CityItem({ city }) {
   const { cityName, emoji, date, id, position } = city;
   console.log(cityName, emoji, date);
+  const { currentCity } = useCities();
+
   return (
     <li>
       <Link
-        className={styles.cityItem}
+        className={`${styles.cityItem} ${
+          id === currentCity.id ? styles["cityItem--active"] : ""
+        }`}
         to={`${id}?lat=${position.lat}&lng=${position.lng}`}
       >
         <span className={styles.emoji}>{emoji}</span>
--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -5,6 +5,8 @@ const CitiesContext = createContext();
 function CitiesProvider({ children }) {
   const [cities, setCities] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [currentCity, setCurrentCity] = useState({});
+
   const BASE_URL = "http://localhost:9000";
 
   useEffect(function () {
@@ -24,11 +26,27 @@ function CitiesProvider({ children }) {
     fetchCities();
   }, []);
 
+  async function getCity(id) {
+    try {
+      setIsLoading(true);
+      const res = await fetch(`${BASE_URL}/cities/${id}`);
+      const data = await res.json();
+      console.log(data);
+      setCurrentCity(data);
+    } catch {
+      alert("There was an Error");
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
   return (
     <CitiesContext.Provider
       value={{
         cities,
         isLoading,
+        currentCity,
+        getCity,
       }}
     >
       {children}
