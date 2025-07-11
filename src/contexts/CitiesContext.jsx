609b8acde167d1ec2612266fd91771bd8428640c
ShahdRaafat <shahdraafat.725@gmail.com>
2025-07-11 23:09:31 +0300

    Create Cities Context

---
+++ src/contexts/CitiesContext.jsx
@@ -0,0 +1,45 @@
+import { createContext, useState, useEffect, useContext } from "react";
+
+const CitiesContext = createContext();
+
+function CitiesProvider({ children }) {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const BASE_URL = "http://localhost:9000";
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        console.log(data);
+        setCities(data);
+      } catch {
+        alert("There was an Error");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
+  return (
+    <CitiesContext.Provider
+      value={{
+        cities,
+        isLoading,
+      }}
+    >
+      {children}
+    </CitiesContext.Provider>
+  );
+}
+function useCities() {
+  const value = useContext(CitiesContext);
+  if (value === undefined)
+    throw new Error("Cities context was used outside Cities Provider");
+  return value;
+}
+
+export { CitiesProvider, useCities };
