9f51321f41b1f4227a7ec791051c8abe47b1028d
ShahdRaafat <shahdraafat.725@gmail.com>
2025-07-11 22:34:20 +0300

    Create Initial Routes and components

---
+++ src/components/Map.jsx
@@ -0,0 +1,20 @@
+import { useNavigate, useSearchParams } from "react-router-dom";
+import styles from "./Map.module.css";
+function Map() {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+  const navigate = useNavigate();
+  return (
+    <div className={styles.mapContainer} onClick={() => navigate("form")}>
+      <h1>
+        Position :{lat},{lng}
+      </h1>
+      <button onClick={() => setSearchParams({ lat: 23, lng: 50 })}>
+        I am here
+      </button>
+    </div>
+  );
+}
+
+export default Map;
